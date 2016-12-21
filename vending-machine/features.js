09bc49ea6e15e58da1785637f4c51286da658243
GSoster <guilhermesosters@gmail.com>
2016-12-21 12:45:18 -0200

    added first test

---
+++ vending-machine/features.js
@@ -0,0 +1,9 @@
+/*
+* totalPayable = how much the item costs
+* cashPaid = how much the user inserted
+* @return change (array)
+*/
+function getChange (totalPayable, cashPaid) {
+  var change = [];
+  return change;
+}
--- vending-machine/tests.js
+++ vending-machine/tests.js
@@ -1,4 +1,16 @@
-test('this sample test should aways pass!', function (assert){
+//Qunit test process: Description -> computation -> acertion
+/*
+* the method getChange should receive 2 arguments (how much an item costs and how much the user inserted)
+* and return the change.
+*/
+test('getChange(1, 1) should equal [] - an empty array', function(assert){
+  var result = getChange(1, 1);
+  var expected = [];
+  assert.deepEqual(result, expected);
+});
+
+//example tests, can be removed later
+/*test('this sample test should aways pass!', function (assert){
   var result = 1 + 1;
   assert.equal(result, 2);//actual result, expected
 });
@@ -8,3 +20,4 @@ test('this is what a failing test looks like!', function(assert){
   var result = [1, 2, 3];indexOf(1);//this should be 0
   assert.equal(result, -1);// we *expect* this to fail
 });
+*/

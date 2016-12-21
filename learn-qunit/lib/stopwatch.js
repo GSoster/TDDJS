ba6b8507b24f51e07b73d39e1fe2ea698242ae96
GSoster <guilhermesosters@gmail.com>
2016-12-21 16:36:26 -0200

    learn QUnit: added initial implementation of startTime method

--- learn-qunit/lib/stopwatch.js
+++ learn-qunit/lib/stopwatch.js
@@ -3,9 +3,16 @@ var T = (function () {
   var timeElapsed = 0;//number of miliseconds since timerStarted
   var timeStarted = 0;//timestamp when timer was started
 
+  var startTimer = function (startTime) {
+    timeStarted = startTime;
+    console.log("Start time: " + startTime);
+    return timeStarted;
+  };
+
  //allow external access to private variables & methods by returning them:
   return {
     timeStarted: timeStarted,
-    timeElapsed: timeElapsed
+    timeElapsed: timeElapsed,
+    startTimer: startTimer
   };
 }());

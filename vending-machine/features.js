1e1db50cead67e830f9375ec8fa7b5d3bbc80595
GSoster <guilhermesosters@gmail.com>
2016-12-21 12:54:52 -0200

    addded a test to check if all coins are being used

--- vending-machine/features.js
+++ vending-machine/features.js
@@ -4,6 +4,7 @@
 * @return change (array)
 */
 function getChange (totalPayable, cashPaid) {
+  var coins = [200, 100, 50, 20, 10, 5, 2, 1];
   var diff = totalPayable - cashPaid;
   var change = [50, 20, 10, 5];
   return change;
--- vending-machine/tests.js
+++ vending-machine/tests.js
@@ -34,6 +34,13 @@ test('getChange(486, 600) should equal [100, 10, 2, 2]', function (assert) {
   assert.deepEqual(result, expected);
 });
 
+
+test('getChange(12, 400) should return [200, 100, 50, 20, 5, 2, 1]', function (assert) {
+  var result = getChange(12, 400);
+  var expected = [200, 100, 50, 20, 5, 2, 1];
+  assert.deepEqual(result, expected);
+});
+
 //#########################################################
 //example tests, can be removed later
 /*test('this sample test should aways pass!', function (assert){

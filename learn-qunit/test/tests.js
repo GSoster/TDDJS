c4202b37d345bce3c1ed2140f88cb8aea3ca1367
GSoster <guilhermesosters@gmail.com>
2016-12-21 16:46:59 -0200

    learn QUnit: added test to stopTimer

--- learn-qunit/test/tests.js
+++ learn-qunit/test/tests.js
@@ -10,3 +10,12 @@ test('startTimer() starts counting from *NOW* (when instructed)', function (){
   var startTime = new Date().getTime();
   equal(T.startTimer(startTime), startTime, true);
 });
+
+
+test('stopTimer() stops counting after 2 seconds', function(){
+  var now = new Date().getTime();
+  var expected =  now + 2000;//2s
+  T.startTimer(now);
+  var result = T.stopTimer(expected);//2s
+  equal(result, expected, true);
+});

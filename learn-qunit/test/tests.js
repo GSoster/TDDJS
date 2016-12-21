ddd6ba18604748cd47230197937671309fd9a716
GSoster <guilhermesosters@gmail.com>
2016-12-21 16:32:13 -0200

    learn QUnit: added test to check if startTimer actually starts from the current time passed as parameter

--- learn-qunit/test/tests.js
+++ learn-qunit/test/tests.js
@@ -5,3 +5,8 @@ test('timeElapsed should be zero before we start the Timer', function (assert) {
   var expected = 0;
   equal(result, expected, 'Message: Timer is zero at start');
 });
+
+test('startTimer() starts counting from *NOW* (when instructed)', function (){
+  var startTime = new Date().getTime();
+  equal(T.startTimer(startTime), startTime, true);
+});
